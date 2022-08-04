ef7167333eab61b8624ae506cb9ede734cf7b23c
Charlie Humphreys <chumphreys@mozilla.com>
2022-08-04 09:28:40 -0500

    Update the Dockerfile, action, and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+cd $1
+git checkout -B automation/update-experiments-json
+curl https://experimenter.services.mozilla.com/api/v6/experiments/?is_first_run=True | jq '{"data":map(select(.appName == "fenix"))}' > $2
+export CHANGED=$(git status | grep '$2' | wc -l)
+
+if test $CHANGED -eq 1
+then
+git add $2
+git commit -m "update initial_experiments.json based on the first run experiments in experimenter"
+echo "$CHANGED file(s) have been modified"
+else
+echo "No changes to make, exiting job"
+fi
+echo "::set-output name=changed::$CHANGED"
