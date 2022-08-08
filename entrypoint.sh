5b4220a294c1f7b80d2a23bb1a3249238ea4b81c
Charlie Humphreys <chumphreys@mozilla.com>
2022-08-08 16:23:16 -0500

    remove commit and add safe directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
 cd $1
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
 git fetch
 git checkout -B automation/update-experiments-json
 curl https://experimenter.services.mozilla.com/api/v6/experiments/?is_first_run=True | jq '{"data":map(select(.appName == "fenix"))}' > $2
@@ -9,7 +10,6 @@ export CHANGED=$(git status | grep '$2' | wc -l)
 if test $CHANGED -eq 1
 then
 git add $2
-git commit -m "update initial_experiments.json based on the first run experiments in experimenter"
 echo "$CHANGED file(s) have been modified"
 else
 echo "No changes to make, exiting job"
