9cbfadf0803ae61e55bdf97c98eb2e93b0e637aa
Charlie Humphreys <chumphreys@mozilla.com>
2022-08-08 16:28:48 -0500

    add status call and shift safe directory to first command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh -l
 
+git config --global --add safe.directory "$GITHUB_WORKSPACE/$1"
+
 cd $1
-git config --global --add safe.directory "$GITHUB_WORKSPACE"
 git fetch
 git checkout -B automation/update-experiments-json
 curl https://experimenter.services.mozilla.com/api/v6/experiments/?is_first_run=True | jq '{"data":map(select(.appName == "fenix"))}' > $2
+
+git status
 export CHANGED=$(git status | grep '$2' | wc -l)
 
 if test $CHANGED -eq 1
