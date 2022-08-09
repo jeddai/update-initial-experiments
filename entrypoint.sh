6167f340d37708642ac915e16a962794405f31bf
Charlie Humphreys <chumphreys@mozilla.com>
2022-08-09 08:35:15 -0500

    add inputs for the branch and experiementer url

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,16 @@
 git config --global --add safe.directory "/github/workspace/$1"
 
 cd $1
-git fetch origin automation/update-experiments-json
-git checkout -B automation/update-experiments-json
-curl https://experimenter.services.mozilla.com/api/v6/experiments/?is_first_run=True | jq '{"data":map(select(.appName == "fenix"))}' > $2
+git fetch origin $2
+git checkout -B $2
+curl $4 | jq '{"data":map(select(.appName == "fenix"))}' > $3
 
 git status
-export CHANGED=$(git status | grep '$2' | wc -l)
+export CHANGED=$(git status | grep '$3' | wc -l)
 
 if test $CHANGED -eq 1
 then
-git add $2
+git add $3
 echo "$CHANGED file(s) have been modified"
 else
 echo "No changes to make, exiting job"
