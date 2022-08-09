cc90a27b6db77727c410b4055b48bec8fb196ebc
Charlie Humphreys <chumphreys@mozilla.com>
2022-08-09 08:53:29 -0500

    update changed variable syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ git config --global --add safe.directory "/github/workspace/$1"
 cd $1
 git fetch origin $2
 git checkout -B $2
-curl $4 | jq '{"data":map(select(.appName == "fenix"))}' > $3
+curl $4 | jq '{"data":map(select(.appName == "fenix"))}' >  $3
 
 git status
-export CHANGED=$(git status | grep '$3' | wc -l)
+export CHANGED=$(git status -s | wc -l)
 
 if test $CHANGED -eq 1
 then
