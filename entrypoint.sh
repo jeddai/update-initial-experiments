bcb32d45b348ef234aaeda1736cdd91a25096940
Charlie Humphreys <chumphreys@mozilla.com>
2023-01-09 14:44:12 -0600

    update regex to escape . character

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ git config --global --add safe.directory "/github/workspace/$1"
 cd $1
 export DATA=$(curl $3 | jq --arg APP_NAME "$4" '{"data":map(select(.appName == $APP_NAME))}')
 
-if [[ $2 =~ .*.json ]]; then
+if [[ $2 =~ .*\.json ]]; then
     echo -e "$DATA" > $2
-elif [[ $2 =~ .*.swift ]]; then
+elif [[ $2 =~ .*\.swift ]]; then
     echo -e "// This Source Code Form is subject to the terms of the Mozilla Public\n// License, v. 2.0. If a copy of the MPL was not distributed with this\n// file, You can obtain one at http://mozilla.org/MPL/2.0/\n\nimport Foundation\n\npublic struct FirstRunExperiments {\n    public static let value = \"\"\"$DATA\"\"\"\n}" > $2
-elif [[ $2 =~ .*.kt ]]; then
+elif [[ $2 =~ .*\.kt ]]; then
     if [[ -z $6 ]]; then
         echo "Package is not specified. Package is a required value for the kotlin language."
         exit 1
