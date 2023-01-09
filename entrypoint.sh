b1d72e7575acf9888712ce20f9f83da2307f22a2
Charlie Humphreys <chumphreys@mozilla.com>
2023-01-09 14:15:24 -0600

    update script to write kotlin and swift files as well as JSON

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,22 @@
 git config --global --add safe.directory "/github/workspace/$1"
 
 cd $1
-curl $3 | jq --arg APP_NAME "$4" '{"data":map(select(.appName == $APP_NAME))}' > $2
+export DATA=$(curl $3 | jq --arg APP_NAME "$4" '{"data":map(select(.appName == $APP_NAME))}')
+
+if [[ $6 == "json" ]]; then
+    echo -e "$DATA" > $2
+elif [[ $6 == "swift" ]]; then
+    echo -e "// This Source Code Form is subject to the terms of the Mozilla Public\n// License, v. 2.0. If a copy of the MPL was not distributed with this\n// file, You can obtain one at http://mozilla.org/MPL/2.0/\n\nimport Foundation\n\npublic struct FirstRunExperiments {\n    public static let value = \"\"\"$DATA\"\"\"\n}" > $2
+elif [[ $6 == "kotlin" ]]; then
+    if [[ -z $7 ]]; then
+        echo "Package is not specified. Package is a required value for the kotlin language."
+        exit 1
+    fi
+    echo -e "package $7\n\nconst val firstRunExperiments = \"\"\"$DATA\"\"\"" > $2
+else
+    echo "Language $6 is not supported. Please choose between 'kotlin', 'swift', or 'json'"
+    exit 1
+fi
 
 git status
 export CHANGED=$(git status -s | wc -l)
